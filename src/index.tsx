4d9c1755758654a2a96207a318eafcfd9dc6e7a1
reyimu <reyimu@bytedance.com>
2021-12-22 20:59:10 +0800

    feat: 实例代码

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+// 引入全局样式
+import './styles/index.scss'
 
 library.add(fas);
 
@@ -13,3 +19,15 @@ export { default as Progress } from "./components/Progress";
 export { default as Tabs } from "./components/Tabs";
 export { default as Transition } from "./components/Transition";
 export { default as Upload } from "./components/Upload";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
